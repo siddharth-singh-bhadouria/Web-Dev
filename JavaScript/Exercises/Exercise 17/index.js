d4bc388dcf6c381c4be60d3ca36e3c6f4a35bbd7
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-04-17 15:52:29 +0530

    Commit on 17/04/2023

---
+++ JavaScript/Exercises/Exercise 17/index.js
@@ -0,0 +1,13 @@
+// Write a loop that prints:
+// 25
+// 20
+// 15
+// 10
+// 5
+// 0
+
+
+
+for (let i = 5; i >= 0; i--) {
+    console.log(i * 5);
+}
--- JavaScript/Loops/WhileLoop/app.js
+++ JavaScript/Loops/WhileLoop/app.js
@@ -24,29 +24,28 @@
 //     if (i === 100) break;
 // }
 
-let maximum = parseInt(prompt("Enter the maximum number!"));
+let maximum = parseInt(prompt('Enter the maximum number'));
 while (!maximum) {
-    maximum = parseInt(prompt("Enter a valid number!"));
+    maximum = parseInt(prompt('Enter a valid choice!'));
 }
+
+let c = 1;
 
 const targetNum = Math.floor(Math.random() * maximum) + 1;
 
-let guess = (prompt("Enter your first guess!"));
-let attempts = 1;
+let guess = prompt('Enter your first guess');
 
 while (parseInt(guess) !== targetNum) {
-    if (guess === 'q') break;
-    attempts++;
-    if (guess > targetNum) {
-        guess = prompt("Too high! Enter a new guess:");
-    } else {
-        guess = prompt("Too low! Enter a new guess:");
-    }
+    if (guess === 'q')
+        break;
+    c++;
+    if (guess > targetNum)
+        guess = prompt('Too high!!Enter your next guess');
+    else
+        guess = prompt('Too low!!Enter your next guess');
 }
 
-if (guess === 'q') {
-    console.log("OK, YOU QUIT!")
-} else {
-    console.log("CONGRATS YOU WIN!")
-    console.log(`You got it! It took you ${attempts} guesses`)
-}
+if (guess === 'q')
+    console.log('OK, YOU QUIT!');
+else
+    console.log(`Congratulations!!You took ${c} guesses`);
