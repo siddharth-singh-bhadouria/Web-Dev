ad93e0c721fddce2319b77ff932b4ec4d632417f
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-05-15 19:15:30 +0530

    Commit 2 on 15/05/2023

---
+++ JavaScript/Loops/ForOf/app.js
@@ -0,0 +1,37 @@
+const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+
+// for (let i = 0; i < subreddits.length; i++) {
+//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// }
+
+
+for (let subreddit of subreddits) {
+    console.log(`Visit reddit.com/r/${subreddit}`)
+}
+
+
+
+// const seatingChart = [
+//     ['Kristen', 'Erik', 'Namita'],
+//     ['Geoffrey', 'Juanita', 'Antonio', 'Kevin'],
+//     ['Yuma', 'Sakura', 'Jack', 'Erika']
+// ]
+
+
+// // for (let i = 0; i < seatingChart.length; i++) {
+// //     for (let j = 0; j < seatingChart[i].length; j++) {
+// //         console.log(seatingChart[i][j]);
+// //     }
+// // }
+
+
+// for (let row of seatingChart) {
+//     for (let column of row) {
+//         console.log(column);
+//     }
+// }
+
+
+// for (let char of 'hello world') {
+//     console.log(char);
+// }
---
+++ JavaScript/Loops/Iterating Objects/app.js
@@ -0,0 +1,15 @@
+const testScores = {
+    keenan: 80,
+    damon: 67,
+    kim: 89,
+    shawn: 91,
+    marlon: 72,
+    dwayne: 77,
+    nadia: 83,
+    elvira: 97,
+    diedre: 81,
+    vonnie: 60
+}
+// ===================================
+// USING FOR...IN TO ITERATE AN OBJECT
+// ===================================
