08c08a253e6465e5f1085102090ecc71fb7c9e85
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-05-28 00:14:01 +0530

    Commit on 28/05/2023

---
+++ JavaScript/Functions/Intro/app.js
@@ -0,0 +1,43 @@
+
+function singSong() {
+    console.log('Do');
+    console.log('Re');
+    console.log('Mi');
+}
+
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+
+function repeat(str, numTimes) {
+    let result = '';
+    for (let i = 0; i < numTimes; i++) {
+        result += str;
+    }
+    console.log(result);
+}
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    return x + y;
+}
+
+
+
+
+
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+// singSong()
+
