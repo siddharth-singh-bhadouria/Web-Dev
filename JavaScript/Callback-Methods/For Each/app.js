d9efa6a5e48b0f9d217c3c303ac7eb7225b15cf7
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-06-05 23:18:49 +0530

    Commit on 05/06/2023

---
+++ JavaScript/Callback-Methods/For Each/app.js
@@ -0,0 +1,28 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+
+
+
+
+// for (let el of numbers) {
+//     console.log(el);
+// }
+
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
