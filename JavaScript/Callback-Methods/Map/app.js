453ce3855dc41eb844d2dc7578f5ccfda089dbbb
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-06-08 02:10:05 +0530

    Commit 2 on 08/06/2023

--- JavaScript/Callback-Methods/Map/app.js
+++ JavaScript/Callback-Methods/Map/app.js
@@ -1,6 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
-
+const double = numbers.map(function (num) {
+    return 2 * num;
+})
 
 
 const movies = [
@@ -21,3 +23,8 @@ const movies = [
         score: 90
     }
 ]
+
+
+const titles = movies.map(function (movie) {
+    return movie.title.toUpperCase();
+})
