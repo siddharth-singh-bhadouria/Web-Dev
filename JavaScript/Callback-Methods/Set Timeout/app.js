9c9037dffba9f4201e0b06e8419a144bd2d4c0fe
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-06-08 03:08:06 +0530

    Commit 5 on 08/06/2023

--- JavaScript/Callback-Methods/Set Timeout/app.js
+++ JavaScript/Callback-Methods/Set Timeout/app.js
@@ -1,23 +1,13 @@
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+console.log('HELLO!!!...')
+setTimeout(() => {
+    console.log('...are you still there?')
+}, 3000)
+
+console.log('GOODBYE!!')
 
 
+const id = setInterval(() => {
+    console.log(Math.random())
+}, 2000)
 
-
-const movies = [
-    {
-        title: 'Amadeus',
-        score: 99
-    },
-    {
-        title: 'Stand By Me',
-        score: 85
-    },
-    {
-        title: 'Parasite',
-        score: 95
-    },
-    {
-        title: 'Alien',
-        score: 90
-    }
-]
+// clearInterval(id)
