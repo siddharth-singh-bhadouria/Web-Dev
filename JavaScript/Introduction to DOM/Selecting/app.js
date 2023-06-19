af241abb834724c4774048e8a56c17f7b1f24bfe
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-06-19 23:46:22 +0530

    Commit 5 on 19/06/2023

---
+++ JavaScript/Introduction to DOM/Selecting/app.js
@@ -0,0 +1,14 @@
+
+// const allImages = document.getElementsByTagName('img')
+
+// for (img of allImages) {
+//     img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+// }
+
+
+const squareImages = document.getElementsByClassName('square')
+
+for (img of squareImages) {
+    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+}
+
