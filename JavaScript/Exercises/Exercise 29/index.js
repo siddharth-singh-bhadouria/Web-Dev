ea165afa9bc340a63da9d13dbb3edfbc56f039e4
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-07 09:38:07 +0530

    Commit on 07/07/2023

---
+++ JavaScript/Exercises/Exercise 29/index.js
@@ -0,0 +1,18 @@
+
+// function sumArray(arr) {
+//     let total = 0;
+//     for (element of arr) {
+//         total += element;
+//     }
+//     return total;
+// }
+
+
+const creature = "Common Sea Dragon";
+
+function scubaDive() {
+    const creature = "Spanish Dancer"; //A type of sea slug
+    console.log(creature);
+}
+
+scubaDive();
