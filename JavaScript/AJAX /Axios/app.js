b8883c4626d999493cc396872e2994c72d21b4ad
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-07 16:38:10 +0530

    Commit 5 on 07/07/2023

--- JavaScript/AJAX /Axios/app.js
+++ JavaScript/AJAX /Axios/app.js
@@ -20,14 +20,37 @@
 //         console.log('ERROR!!', e)
 //     })
 
-const getBitcoinPrice = async () => {
+// const getBitcoinPrice = async () => {
+//     try {
+//         const res = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+//         console.log(res.data.USD)
+//     }
+//     catch (e) {
+//         console.log("ERROR!", e)
+//     }
+// }
+
+// getBitcoinPrice()
+
+const jokes = document.querySelector('#jokes')
+const button = document.querySelector('button')
+
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke()
+    const newLI = document.createElement('LI')
+    newLI.append(jokeText)
+    jokes.append(newLI)
+}
+
+const getDadJoke = async () => {
     try {
-        const res = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
-        console.log(res.data.USD)
+        const config = { headers: { Accept: 'application/json' } }
+        const res = await axios.get('https://icanhazdadjoke.com/', config)
+        return res.data.joke
     }
     catch (e) {
-        console.log("ERROR!", e)
+        return 'NO JOKES AVAILABLE! SORRY :('
     }
 }
 
-getBitcoinPrice()
+button.addEventListener('click', addNewJoke)
