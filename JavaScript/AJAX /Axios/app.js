b1c26dafecc9db84cea2a85a951f72c5950857fc
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-07 16:58:47 +0530

    Commit 6 on 07/07/2023

--- JavaScript/AJAX /Axios/app.js
+++ JavaScript/AJAX /Axios/app.js
@@ -20,17 +20,17 @@
 //         console.log('ERROR!!', e)
 //     })
 
-// const getBitcoinPrice = async () => {
-//     try {
-//         const res = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
-//         console.log(res.data.USD)
-//     }
-//     catch (e) {
-//         console.log("ERROR!", e)
-//     }
-// }
+const getBitcoinPrice = async () => {
+    try {
+        const res = await axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+        console.log(res.data.USD)
+    }
+    catch (e) {
+        console.log("ERROR!", e)
+    }
+}
 
-// getBitcoinPrice()
+getBitcoinPrice()
 
 const jokes = document.querySelector('#jokes')
 const button = document.querySelector('button')
