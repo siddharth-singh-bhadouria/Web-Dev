03f12b004620554810afd55f149da684207e557f
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-11 15:33:28 +0530

    Commit 3 on 11/07/2023

--- Modules and NPM/Intro/app.js
+++ Modules and NPM/Intro/app.js
@@ -1,3 +1,9 @@
+const allCats = require('../Shelter')
 const { PI, square } = require('./math')
 
-console.log(PI) 
+const cats = require('../Shelter')
+
+// console.log(PI)
+// console.log(square(9))
+
+console.log('Required an entire directory!!', allCats)
---
+++ Modules and NPM/Shelter/index.js
@@ -0,0 +1,8 @@
+
+const blue = require('./blue')
+const janet = require('./janet')
+const sadie = require('./sadie')
+
+const allCats = [blue, janet, sadie]
+
+module.exports = allCats
