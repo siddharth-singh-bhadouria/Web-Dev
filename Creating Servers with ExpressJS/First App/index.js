9e44130b94aae91012b3fd7ac5cbf91d501b5b2f
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-12 00:56:29 +0530

    Commit on 12/07/2023

---
+++ Creating Servers with ExpressJS/First App/index.js
@@ -0,0 +1,42 @@
+
+const express = require("express");
+const app = express();
+
+// app.use((req, res) => {
+//     console.log("WE GOT A NEW REQUEST!!")
+//     res.send('<h1>This is my webpage!</h1>')
+// })
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the home page!')
+})
+
+app.post('/cats', (req, res) => {
+    res.send('POST REQUEST TO /cats!!!! THIS IS DIFFERENT THAN A GET REQUEST!')
+})
+
+app.get('/cats', (req, res) => {
+    res.send('MEOW!!')
+})
+
+app.get('/dogs', (req, res) => {
+    res.send('WOOF!')
+})
+
+app.get('*', (req, res) => {
+    res.send("I don't know that path!")
+})
+
+
+
+
+
+// /cats => 'meow'
+// /dogs => 'woof'
+// '/' 
+
+
+
+app.listen(8080, () => {
+    console.log("LISTENING ON PORT 8080")
+})
