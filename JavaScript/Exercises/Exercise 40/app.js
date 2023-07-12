a1e036c99adea2be5462b64702d1fb1f6e7508b8
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-13 03:33:42 +0530

    Commit 6 on 13/07/2023

---
+++ JavaScript/Exercises/Exercise 40/app.js
@@ -0,0 +1,5 @@
+// The url you need: 'https://devsprouthosting.com/images/chicken.jpg'
+
+const img = document.querySelector('img')
+img.src = 'https://devsprouthosting.com/images/chicken.jpg'
+img.alt = 'chicken'
