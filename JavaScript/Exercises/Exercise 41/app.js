fb0ae230aa1626059891ef1b031b66d80fefbbe2
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-13 03:40:35 +0530

    Commit 7 on 13/07/2023

---
+++ JavaScript/Exercises/Exercise 41/app.js
@@ -0,0 +1,7 @@
+
+const div = document.querySelector('#container')
+div.style.textAlign = 'center'
+
+const img = document.querySelector('img')
+img.style.width = '150px'
+img.style.borderRadius = '50%'
