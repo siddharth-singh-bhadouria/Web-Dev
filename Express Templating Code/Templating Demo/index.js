a1714dedf0c09aa91673dc59c59419a9a84287d4
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-17 00:17:32 +0530

    Commit 7 on 16/07/2023

--- Express Templating Code/Templating Demo/index.js
+++ Express Templating Code/Templating Demo/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const redditData = require('./data.json')
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, '/views'))
@@ -16,7 +17,14 @@ app.get('/rand', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params
-    res.render('subreddit', { subreddit })
+    const data = redditData[subreddit]
+    // console.log(data)
+    if (data) {
+        res.render('subreddit', { ...data })
+    }
+    else {
+        res.render('notfound', { subreddit })
+    }
 })
 
 app.get('/cats', (req, res) => {
