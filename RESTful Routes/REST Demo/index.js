8c2a418e741c8fb1b47c5f419f51548f552bb838
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-17 20:24:01 +0530

    Commit 4 on 17/07/2023

--- RESTful Routes/REST Demo/index.js
+++ RESTful Routes/REST Demo/index.js
@@ -1,8 +1,36 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+// Our fake database:
+let comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8erBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('index', { comments })
+})
 
 app.get('/tacos', (req, res) => {
     res.send('GET /tacos response')
