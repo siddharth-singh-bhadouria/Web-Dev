7d5f1eafe0652e7f6635ed04f2c3264e00a0254e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-18 02:31:28 +0530

    Commit 2 on 18/07/2023

--- JavaScript/Exercises/Exercise 42/app.js
+++ JavaScript/Exercises/Exercise 42/app.js
@@ -1,3 +1,8 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
 
 //YOU CODE GOES HERE:
+
+let spans = document.querySelectorAll('span')
+for (let i = 0; i < colors.length; i++) {
+    spans[i].style.color = colors[i]
+}
