4aab4d318373bb41f8c2267283a7d60a1c9504a0
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-19 18:49:31 +0530

    Commit 2 on 19/07/2023

--- Mongoose Basics/index.js
+++ Mongoose Basics/index.js
@@ -1,4 +1,4 @@
-// // getting-started.js
+// getting-started.js
 // const mongoose = require('mongoose');
 
 // main().catch(err => console.log(err));
@@ -13,15 +13,36 @@
 
 
 //INSTEAD OF THE ABOVE , SIMPLY USE .then() and .catch() blocks as mongoose.connect supports promises. 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/test');
-then(() => {
-    console.log('CONNECTION OPEN!!')
-})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp')
+    .then(() => {
+        console.log('CONNECTION OPEN!!')
+    })
     .catch((err) => {
         console.log('OH NO ERROR!!')
         console.log(err)
     })
 
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+})
 
+const Movie = mongoose.model('Movie', movieSchema)
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' })
+amadeus.save()
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+    .then((data) => {
+        console.log('IT WORKED!!')
+        console.log(data)
+    })
