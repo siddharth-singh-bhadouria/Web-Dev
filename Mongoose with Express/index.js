ee74f73975c2b8b0155bf98cc5d336df3e6549d8
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 02:49:19 +0530

    Commit 4 on 21/07/2023

--- Mongoose with Express/index.js
+++ Mongoose with Express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 
 const Product = require('./models/product')
 
@@ -18,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
@@ -43,6 +45,13 @@ app.get('/products/:id', async (req, res) => {
     res.render('products/show', { product })
 })
 
+app.get('/products/:id/edit', async(req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit',{product})
+})
+
+app.put()
 
 
 app.listen(3000, () => {
