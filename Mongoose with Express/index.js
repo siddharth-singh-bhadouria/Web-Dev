1f4bbc7becbfa2a632f926014f3f5ff3ee0335b1
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 02:06:27 +0530

    Commit 3 on 21/07/2023

--- Mongoose with Express/index.js
+++ Mongoose with Express/index.js
@@ -17,10 +17,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products)
     res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body)
+    await newProduct.save()
+    console.log(newProduct)
+    res.redirect(`/products/${newProduct.id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
