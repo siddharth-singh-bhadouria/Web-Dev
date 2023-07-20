4378191baaf406ca634935af93080f9ce75054b6
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 00:40:18 +0530

    Commit on 21/07/2023

--- Mongoose with Express/index.js
+++ Mongoose with Express/index.js
@@ -7,18 +7,20 @@ const Product = require('./models/product')
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
     .then(() => {
-        console.log('CONNECTION OPEN!!')
+        console.log('MONGO CONNECTION OPEN!!')
     })
     .catch((err) => {
-        console.log('OH NO ERROR!!')
+        console.log('OH NO MONGO CONNECTION ERROR!!')
         console.log(err)
     })
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-app.get('/dogs', (req, res) => {
-    res.send('WOOF!!')
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    console.log(products)
+    res.render('products/index', { products })
 })
 
 app.listen(3000, () => {
