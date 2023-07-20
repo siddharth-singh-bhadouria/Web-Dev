927facece00af495c4a12be1632b1bf685edd81d
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 01:12:33 +0530

    Commit 2 on 21/07/2023

--- Mongoose with Express/index.js
+++ Mongoose with Express/index.js
@@ -23,6 +23,15 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products })
 })
 
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    console.log(product)
+    res.render('products/show', { product })
+})
+
+
+
 app.listen(3000, () => {
     console.log('LISTENING TO PORT 3000!!')
 }) 
