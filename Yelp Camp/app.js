cfe09f565485b897a65ca8d685eab3aaec601735
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 17:08:03 +0530

    Commit 9 on 21/07/2023

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
+    .then(() => {
+        console.log('MONGO CONNECTION OPEN!!')
+    })
+    .catch((err) => {
+        console.log('OH NO MONGO CONNECTION ERROR!!')
+        console.log(err)
+    })
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'Cheap Camping!!' })
+    await camp.save()
+    res.send(camp)
 })
 
 app.listen(3000, () => {
---
+++ Yelp Camp/models/campground.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const campgroundSchema = new Schema({
+    title: String,
+    price: String,
+    description: String,
+    location: String
+})
+
+const Campground = new mongoose.model('Campground', campgroundSchema)
+
+module.exports = Campground
