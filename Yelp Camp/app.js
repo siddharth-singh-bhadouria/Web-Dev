53c52002d1a237cf9d9403240b025b6ef8220f4f
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 22:28:04 +0530

    Commit 15 on 21/07/2023

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
 
@@ -17,6 +18,7 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -44,6 +46,23 @@ app.get('/campgrounds/:id', async (req, res) => {
     res.render('campgrounds/show', { campground })
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findById(id)
+    res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    res.redirect(`/campgrounds/${campground.id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')
+})
 
 
 app.listen(3000, () => {
