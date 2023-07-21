dd61273674037f804616679b7a2de2bfc7cce412
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-21 19:55:20 +0530

    Commit 12 on 21/07/2023

--- Yelp Camp/seeds/index.js
+++ Yelp Camp/seeds/index.js
@@ -20,7 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const camp = new Campground({
             location: `${cities[random1000].city} , ${cities[random1000].state}`,
-            title: `${sample(descriptors)} , ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`
         })
         await camp.save()
     }
