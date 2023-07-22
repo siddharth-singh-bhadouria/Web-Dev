40cfcb08de3bb6e250b4dd5e8ca1523fa76856be
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-22 21:16:31 +0530

    Commit 4 on 22/07/2023

--- Yelp Camp/seeds/index.js
+++ Yelp Camp/seeds/index.js
@@ -18,9 +18,13 @@ const seedDB = async () => {
     await Campground.deleteMany({})
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000)
+        const price = Math.floor(Math.random() * 20) + 10
         const camp = new Campground({
             location: `${cities[random1000].city} , ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'http://source.unsplash.com/collection/484351',
+            description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, eum nostrum ipsam odio ratione repudiandae odit mollitia impedit quod quam sunt aspernatur? Quidem aliquid facilis accusamus ducimus nam iure sequi!',
+            price
         })
         await camp.save()
     }
