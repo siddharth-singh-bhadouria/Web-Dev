c9097939f7d24bfa9d3eb1f6cf1e998e43a1e0ea
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-22 18:47:45 +0530

    Commit 2 on 22/07/2023

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const Campground = require('./models/campground')
@@ -14,6 +15,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
         console.log(err)
     })
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
