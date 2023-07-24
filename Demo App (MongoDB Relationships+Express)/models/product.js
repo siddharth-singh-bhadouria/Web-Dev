7c8010b0c9db676f39f4fd324e592c6a8f0861dc
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-24 21:54:55 +0530

    Commit 5 on 24/07/2023

--- Demo App (MongoDB Relationships+Express)/models/product.js
+++ Demo App (MongoDB Relationships+Express)/models/product.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const { Schema } = mongoose
 
-const productSchema = new mongoose.Schema({
+const productSchema = new Schema({
     name: {
         type: String,
         required: true
@@ -14,8 +15,11 @@ const productSchema = new mongoose.Schema({
         type: String,
         lowercase: true,
         enum: ['fruit', 'vegetable', 'dairy']
+    },
+    farm: {
+        type: Schema.Types.ObjectId,
+        ref: 'Farm'
     }
-
 })
 
 const Product = new mongoose.model('Product', productSchema)
