be63f6468e7c37845b90355e802d3cb3a78a9630
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-24 17:48:05 +0530

    Commit on 24/07/2023

--- Mongo Relationships/models/farm.js
+++ Mongo Relationships/models/farm.js
@@ -34,23 +34,28 @@ const Farm = new mongoose.model('Farm', farmSchema)
 //     { name: 'Asparagus', price: 3.99, season: 'Spring' },
 // ])
 
-const makeFarm = async () => {
-    const farm = new Farm({ name: 'Full Belly Farm', city: 'Guinda,CA' })
-    const melon = await Product.findOne({ name: 'Goddess Melon' })
-    farm.products.push(melon)
-    await farm.save()
-    console.log(farm)
-}
-
-makeFarm()
-
-// const addProduct = async () => {
-//     const farm = await Farm.findOne({ name: 'Full Belly Farm' })
-//     const watermelon = await Product.findOne({ name: 'Sugar Baby Watermelon' })
-//     farm.products.push(watermelon)
+// const makeFarm = async () => {
+//     const farm = new Farm({ name: 'Full Belly Farm', city: 'Guinda,CA' })
+//     const melon = await Product.findOne({ name: 'Goddess Melon' })
+//     farm.products.push(melon)
 //     await farm.save()
 //     console.log(farm)
 // }
 
+// makeFarm()
+
+const addProduct = async () => {
+    const farm = await Farm.findOne({ name: 'Full Belly Farm' })
+    const watermelon = await Product.findOne({ name: 'Sugar Baby Watermelon' })
+    farm.products.push(watermelon)
+    await farm.save()
+    console.log(farm)
+}
+
 // addProduct()
 
+Farm.findOne({ name: 'Full Belly Farm' })
+    .populate('products')
+    .then((farm) => {
+        console.log(farm)
+    })
