d15aea625c5a100ec40449add2bcd3ff0b99be84
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-24 22:02:56 +0530

    Commit 6 on 24/07/2023

--- Demo App (MongoDB Relationships+Express)/index.js
+++ Demo App (MongoDB Relationships+Express)/index.js
@@ -21,6 +21,19 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+
+// FARM ROUTES
+
+app.get('/farms/new', (req, res) => {
+    res.render('farms/new')
+})
+
+
+
+
+
+
+// PRODUCT ROUTES
 const categories = ['fruit', 'vegetable', 'dairy']
 
 app.get('/products', async (req, res) => {
