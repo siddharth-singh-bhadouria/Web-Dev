5f9e3a111a5bf8ccacd08f044f68f266df02675e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-25 22:34:37 +0530

    Commit 5 on 25/07/2023

--- Yelp Camp/app.js
+++ Yelp Camp/app.js
@@ -75,7 +75,7 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { status = 500, message = 'Something went wrong' } = err
-    res.status(status).send(message)
+    res.status(status).render('error')
 })
 
 
