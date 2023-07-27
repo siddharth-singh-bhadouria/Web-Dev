4a01e829a1d1f04a740b922048520d812926e31e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-27 20:42:28 +0530

    Commit 2 on 27/07/2023

--- Demo App (MongoDB Relationships+Express)/index.js
+++ Demo App (MongoDB Relationships+Express)/index.js
@@ -54,14 +54,14 @@ app.get('/farms/:id/products/new', (req, res) => {
 
 app.post('/farms/:id/products', async (req, res) => {
     const { id } = req.params
-    const farm = await Farm(req.body)
-    const { name, city, email } = farm
-    const product = new Product({ name, city, email })
+    const farm = await Farm.findById(id)
+    const { name, price, category } = req.body
+    const product = new Product({ name, price, category })
     farm.products.push(product)
     product.farm = farm
     await farm.save()
     await product.save()
-    res.send(farm)
+    res.redirect(`/farms/${id}`)
 })
 
 
