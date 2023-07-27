c6c3954a86e8a955ca2a6962330c35b18542216e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-27 23:50:50 +0530

    Commit 6 on 27/07/2023

--- Yelp Camp/models/campground.js
+++ Yelp Camp/models/campground.js
@@ -6,7 +6,13 @@ const campgroundSchema = new Schema({
     image: String,
     price: Number,
     description: String,
-    location: String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
 })
 
 const Campground = new mongoose.model('Campground', campgroundSchema)
