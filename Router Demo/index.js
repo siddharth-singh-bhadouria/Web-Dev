655e33bf3c82bb1a0e951c1979cf22498c29c850
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-29 01:11:13 +0530

    Commit 3 on 29/07/2023

--- Router Demo/index.js
+++ Router Demo/index.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const shelterRoutes = require('./routes/shelters')
+const dogRoutes = require('./routes/dogs')
 
+app.use('/shelters', shelterRoutes)
 
-app.use('shelters', shelterRoutes)
+app.use('/dogs', dogRoutes)
+
+app.listen(3000, () => {
+    console.log('LISTENING TO PORT 3000!!')
+})
