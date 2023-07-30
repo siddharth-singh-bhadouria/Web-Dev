58e27a8c2923858d330a555fa8bb51a035676ae0
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-31 02:32:06 +0530

    Commit on 31/07/2023s

--- Cookies Demo/index.js
+++ Cookies Demo/index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+app.get('/greet', (req, res) => {
+    res.send('HEY THERE!!')
+})
+
+app.get('/setname', (req, res) => {
+    res.cookie('name', 'hendrietta')
+    res.cookie('animal', 'harlequin shrimp')
+    res.send('HEY THERE!!')
+})
 
 app.listen(3000, () => {
     console.log('LISTENING TO PORT 3000!!')
