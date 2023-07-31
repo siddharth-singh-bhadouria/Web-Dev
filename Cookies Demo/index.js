f393be1038e23508baedb293c5bc2ba4d44f4e01
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-07-31 16:08:28 +0530

    Commit 2 on 31/07/2023s

--- Cookies Demo/index.js
+++ Cookies Demo/index.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const app = express()
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser('thisismysecret'))
 
 app.get('/greet', (req, res) => {
-    res.send('HEY THERE!!')
+    const { name = 'No-name' } = req.cookies
+    res.send(`HEY THERE ${name}!!`)
 })
 
 app.get('/setname', (req, res) => {
     res.cookie('name', 'hendrietta')
     res.cookie('animal', 'harlequin shrimp')
     res.send('HEY THERE!!')
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true })
+    res.send('OK , SIGNED YOUR FRUIT COOKIE!!')
+})
+
+app.get('/verifyfruit', (req, res) => {
+    res.send(req.signedCookies)
+    // res.cookie('fruit', 'grape', { signed: true })
+
 })
 
 app.listen(3000, () => {
