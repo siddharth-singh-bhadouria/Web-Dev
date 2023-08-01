d64462de976a9c0dfaf86fd965d3caa32e38f0db
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-01 15:59:45 +0530

    Commit 3 on 01/08/2023

--- Session Demo/index.js
+++ Session Demo/index.js
@@ -16,7 +16,19 @@ app.get('/viewcount', (req, res) => {
     res.send(`You have viewed this page ${req.session.count} times`)
 })
 
-// Different browsers have different session id , thats why they each access different sessions or different session portions .  
+// Different browsers have different session id , thats why they each access different sessions or different session portions .
+
+
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query
+    req.session.username = username
+    res.redirect('/greet')
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session
+    res.send(`Welcome back ${username}`)
+})
 
 app.listen(3000, () => {
     console.log('LISTENING TO PORT 3000!!')
