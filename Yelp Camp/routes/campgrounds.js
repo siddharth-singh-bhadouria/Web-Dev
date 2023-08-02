0cf77f5228591703b3b3e8ac6cac60ff7f21d3c6
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-02 19:17:36 +0530

    Commit 6 on 02/08/2023

--- Yelp Camp/routes/campgrounds.js
+++ Yelp Camp/routes/campgrounds.js
@@ -38,13 +38,20 @@ router.post('/', validateCampground, catchAsync(async (req, res) => {
 router.get('/:id', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id).populate('reviews')
-    console.log(campground)
+    if (!campground) {
+        req.flash('error', 'Cannot find the campground!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/show', { campground })
 }))
 
 router.get('/:id/edit', catchAsync(async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findById(id)
+    if (!campground) {
+        req.flash('error', 'Cannot find the campground!')
+        return res.redirect('/campgrounds')
+    }
     res.render('campgrounds/edit', { campground })
 }))
 
