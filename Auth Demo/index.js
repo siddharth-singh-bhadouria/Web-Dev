cf245d602f762a99a69b7a48c98ed7b46b6ddd30
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-03 14:52:39 +0530

    Commit 5 on 03/08/2023

--- Auth Demo/index.js
+++ Auth Demo/index.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
 const User = require('./models/user')
+
+mongoose.connect('mongodb://127.0.0.1:27017/authDemo')
+    .then(() => {
+        console.log('MONGO CONNECTION OPEN!!')
+    })
+    .catch((err) => {
+        console.log('OH NO MONGO CONNECTION ERROR!!')
+        console.log(err)
+    })
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
 
 app.get('/secret', (req, res) => {
     res.send('THIS IS SECRET! YOU CANNNOT SEE ME UNLESS YOU ARE LOGGED IN!!')
