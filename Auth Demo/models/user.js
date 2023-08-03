44551099ed933f387bbb2c3b573cd85ffa43ef37
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-03 19:39:05 +0530

    Commit 12 on 03/08/2023

--- Auth Demo/models/user.js
+++ Auth Demo/models/user.js
@@ -19,4 +19,11 @@ userSchema.statics.findAndValidate = async function (username, password) {
     return isValid ? foundUser : false
 }
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password'))
+        return next()
+    this.password = await bcrypt.hash(this.password, 12)
+    next()
+})
+
 module.exports = mongoose.model('User', userSchema)  
