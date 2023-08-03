5f28dc92868aa8604e370370f424c736f4a76e27
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-03 19:23:44 +0530

    Commit 11 on 03/08/2023

--- Auth Demo/models/user.js
+++ Auth Demo/models/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const { Schema } = mongoose
 
-const userSchema = {
+const userSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username cannot be blank']
@@ -9,6 +11,12 @@ const userSchema = {
         type: String,
         required: [true, 'Password cannot be blank']
     }
+})
+
+userSchema.statics.findAndValidate = async function (username, password) {
+    const foundUser = await this.findOne({ username })
+    const isValid = await bcrypt.compare(password, foundUser.password)
+    return isValid ? foundUser : false
 }
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('User', userSchema)  
