35ba51e943f425685eb514c314bea8026750c08d
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-07 19:50:24 +0530

    Commit 5 on 07/08/2023

--- Yelp Camp/routes/users.js
+++ Yelp Camp/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
+const passport = require('passport')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -19,8 +20,16 @@ router.post('/register', catchAsync(async (req, res) => {
         req.flash('error', e.message)
         res.redirect('/register')
     }
-
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login')
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome Back!')
+    res.redirect('/campgrounds')
+})
 
 
 
