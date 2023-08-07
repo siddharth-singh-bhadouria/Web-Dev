ae04c8817aeed9a29c39f886fc8838798daebe85
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-08 00:17:55 +0530

    Commit 2 on 08/08/2023

--- Yelp Camp/routes/users.js
+++ Yelp Camp/routes/users.js
@@ -13,8 +13,13 @@ router.post('/register', catchAsync(async (req, res) => {
         const { username, email, password } = req.body
         const user = new User({ username, email })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Welcome to Yelp Camp!')
-        res.redirect('/campgrounds')
+        req.login(registeredUser, err => {
+            if (err) {
+                return next(err)
+            }
+            req.flash('success', 'Welcome to Yelp Camp!')
+            res.redirect('/campgrounds')
+        })
     }
     catch (e) {
         req.flash('error', e.message)
