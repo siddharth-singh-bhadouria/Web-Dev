04440f73fb6f6e2168703e9a4f81fad100feac0d
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-07 16:27:04 +0530

    Commit on 07/08/2023

--- Yelp Camp/index.js
+++ Yelp Camp/index.js
@@ -11,6 +11,9 @@ const Campground = require('./models/campground')
 const Review = require('./models/review')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+
 
 const campgrounds = require('./routes/campgrounds')
 const reviews = require('./routes/reviews')
