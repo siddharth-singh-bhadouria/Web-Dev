df15cfecd47e1a55ef0e5eade71350d587652366
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-08 16:15:57 +0530

    Commit 4 on 08/08/2023

--- Yelp Camp/index.js
+++ Yelp Camp/index.js
@@ -56,6 +56,9 @@ passport.deserializeUser(User.deserializeUser())
 
 
 app.use((req, res, next) => {
+    if (!['/login', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl
+    }
     res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
--- Yelp Camp/routes/users.js
+++ Yelp Camp/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const User = require('../models/user')
 const passport = require('passport')
+const { storeReturnTo } = require('../middleware')
 
 router.get('/register', (req, res) => {
     res.render('users/register')
@@ -28,14 +29,13 @@ router.post('/register', catchAsync(async (req, res) => {
 }))
 
 router.get('/login', (req, res) => {
-    console.log(req.session)
     res.render('users/login')
 })
 
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     req.flash('success', 'Welcome Back!')
-    console.log(req.session)
-    const redirectUrl = req.session.returnTo || '/campgrounds'
+    console.log(res.locals.returnTo)
+    const redirectUrl = res.locals.returnTo || '/campgrounds'
     res.redirect(redirectUrl)
 })
 
