85121cf10bb506ff46d75622b8a1df6f44011b84
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-10 00:51:33 +0530

    Commit 10 on 09/08/2023

--- Yelp Camp/routes/users.js
+++ Yelp Camp/routes/users.js
@@ -5,18 +5,19 @@ const passport = require('passport')
 const { storeReturnTo } = require('../middleware')
 const users = require('../controllers/users')
 
-router.get('/register', users.renderRegister)
 
-router.post('/register', catchAsync(users.register))
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register))
 
-router.get('/login', users.renderLogin)
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-    req.flash('success', 'Welcome Back!')
-    const redirectUrl = res.locals.returnTo || '/campgrounds'
-    delete res.locals.returnTo
-    res.redirect(redirectUrl)
-})
+router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+        req.flash('success', 'Welcome Back!')
+        const redirectUrl = res.locals.returnTo || '/campgrounds'
+        delete res.locals.returnTo
+        res.redirect(redirectUrl)
+    })
 
 router.get('/logout', users.logout)
 
