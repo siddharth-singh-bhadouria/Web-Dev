041cb2f72966d8b5bd41073ce4f550a4d3dc24e6
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-10 22:33:18 +0530

    Commit 5 on 10/08/2023

--- Yelp Camp/routes/campgrounds.js
+++ Yelp Camp/routes/campgrounds.js
@@ -4,11 +4,17 @@ const catchAsync = require('../utils/catchAsync')
 const ExpressError = require('../utils/ExpressError')
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware')
 const campgrounds = require('../controllers/campgrounds')
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files)
+        res.send('It worked!')
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
