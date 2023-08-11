3306a18c8bc2f3ce6df9801dcc00577339ab2d3e
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-11 18:26:46 +0530

    Commit 4 on 11/08/2023 adding images

--- Yelp Camp/controllers/campgrounds.js
+++ Yelp Camp/controllers/campgrounds.js
@@ -11,7 +11,7 @@ module.exports.renderNewForm = (req, res) => {
 
 module.exports.createCampground = async (req, res) => {
     const campground = new Campground(req.body.campground)
-    campground.images = await req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }))
     campground.author = req.user._id
     // if (!campground) throw new ExpressError('Invalid Campground Data', 400)
     await campground.save()
@@ -49,6 +49,9 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }))
+    campground.images.push({ ...imgs })
+    await campground.save()
     req.flash('success', 'Successfully updated campground!')
     res.redirect(`/campgrounds/${campground.id}`)
 }
