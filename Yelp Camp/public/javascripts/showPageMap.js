9530d8dbe57ed897402367667ebb16bb5b760fe2
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-15 19:40:57 +0530

    Commit 5 on 15/08/2023

--- Yelp Camp/public/javascripts/showPageMap.js
+++ Yelp Camp/public/javascripts/showPageMap.js
@@ -2,6 +2,11 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: JSON.parse(campground).geometry.coordinates, // starting position [lng, lat]
     zoom: 8, // starting zoom
 })
+
+const marker = new mapboxgl.Marker()
+    .setLngLat(JSON.parse(campground).geometry.coordinates)
+    .addTo(map);
+
