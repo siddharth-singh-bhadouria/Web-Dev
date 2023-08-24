c6e0035eb46eae43e1fa05a314c9b95d68de4501
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-24 19:36:29 +0530

    Commit 3 on 24/08/2023 updating port option

--- Yelp Camp/index.js
+++ Yelp Camp/index.js
@@ -155,7 +155,8 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err })
 })
 
+const port = process.env.port || 3000
 
-app.listen(3000, () => {
-    console.log('LISTENING TO PORT 3000!!')
+app.listen(port, () => {
+    console.log(`LISTENING TO PORT ${port}!!`)
 }) 
