e8d8bef91d2b0a4d48663953a6f0273e01c6631b
siddharth-singh-bhadouria <sidsingh.singh01@gmail.com>
2023-08-25 00:13:44 +0530

    Fixed JSON bug

--- Yelp Camp/public/javascripts/showPageMap.js
+++ Yelp Camp/public/javascripts/showPageMap.js
@@ -1,19 +1,19 @@
 mapboxgl.accessToken = mapToken;
-const parsedCampground = JSON.parse(campground);
-const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: parsedCampground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 8, // starting zoom
-})
 
-map.addControl(new mapboxgl.NavigationControl())
+const map = new mapboxgl.Map({
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: campground.geometry.coordinates, // starting position [lng, lat]
+  zoom: 8, // starting zoom
+});
+
+map.addControl(new mapboxgl.NavigationControl());
 
 const marker = new mapboxgl.Marker()
-    .setLngLat(parsedCampground.geometry.coordinates)
-    .setPopup(
-        new mapboxgl.Popup({ offset: 25 })
-            .setHTML(`<h3>${parsedCampground.title}</h3><p>${parsedCampground.location}</p>`)
+  .setLngLat(campground.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<h3>${campground.title}</h3><p>${campground.location}</p>`
     )
-    .addTo(map);
-
+  )
+  .addTo(map);
